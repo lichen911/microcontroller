5b7fa3c9bd5d1c8149f1e96779e6bc232bb23b87
lichen911 <rhatch@gmail.com>
2025-04-01 21:49:40 -0400

    Lint and such

--- water_alarm/water_alarm.ino
+++ water_alarm/water_alarm.ino
@@ -1,24 +1,26 @@
-#define FLOW_SENSOR_PIN 4  // GPIO pin connected to the sensor's data pin
+#define FLOW_SENSOR_PIN 4 // GPIO pin connected to the sensor's data pin
 #define BUZZER_PIN 2
 
-volatile int flowCount = 0;  // To count the number of pulses
+volatile int flowCount = 0; // To count the number of pulses
 bool alarmActive = false;
 int alarmThreshold = 20;
 int activeSeconds = 0;
-unsigned long lastTime = 0;  // Time tracking for flow rate calculation
+unsigned long lastTime = 0; // Time tracking for flow rate calculation
 
 unsigned long lastChimeTime = 0;
-int chimeDuration = 100;  // ms
+int chimeDuration = 100; // ms
 int initChimeIntervalDelay = 3000;
 int chimeIntervalDelay = initChimeIntervalDelay;
 int chimeFreq1 = 880;
 int chimeFreq2 = 1047;
 
-void IRAM_ATTR flowInterrupt() {
-  flowCount++;  // Increment pulse count for each flow
+void IRAM_ATTR flowInterrupt()
+{
+  flowCount++; // Increment pulse count for each flow
 }
 
-void setup() {
+void setup()
+{
   // Start Serial communication for debugging
   Serial.begin(9600);
 
@@ -30,24 +32,31 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(FLOW_SENSOR_PIN), flowInterrupt, RISING);
 }
 
-void doChime(int pin) {
-  if (millis() - lastChimeTime >= chimeIntervalDelay) {
+void doChime(int pin)
+{
+  if (millis() - lastChimeTime >= chimeIntervalDelay)
+  {
     lastChimeTime = millis();
     tone(pin, chimeFreq1, chimeDuration);
     tone(pin, chimeFreq2, chimeDuration);
   }
 }
 
-void loop() {
+void loop()
+{
   // Every second, calculate and print the flow rate
-  if (millis() - lastTime >= 1000) {
+  if (millis() - lastTime >= 1000)
+  {
     Serial.print("Flow count: ");
     Serial.println(flowCount);
 
-    if (flowCount >= alarmThreshold) {
+    if (flowCount >= alarmThreshold)
+    {
       alarmActive = true;
       activeSeconds++;
-    } else {
+    }
+    else
+    {
       alarmActive = false;
       activeSeconds = 0;
     }
@@ -57,17 +66,20 @@ void loop() {
     lastTime = millis();
 
     // Decrease chime interval
-    if (activeSeconds > 0 && activeSeconds % 10 == 0) {
+    if (activeSeconds > 0 && activeSeconds % 10 == 0)
+    {
       chimeIntervalDelay = chimeIntervalDelay / 2;
       Serial.println(chimeIntervalDelay, activeSeconds % 10 == 0);
     }
-    if (activeSeconds == 0 && chimeIntervalDelay != initChimeIntervalDelay) {
+    if (activeSeconds == 0 && chimeIntervalDelay != initChimeIntervalDelay)
+    {
       chimeIntervalDelay = initChimeIntervalDelay;
     }
   }
 
   // Do chime if it's time
-  if (alarmActive) {
+  if (alarmActive)
+  {
     doChime(BUZZER_PIN);
   }
 }
